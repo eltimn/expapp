08487b68a4b9b1146d20d53343d382af45b70d2a
Tim Nelson <tim@eltimn.com>
2022-06-15 11:23:33 -0500

    Fix remve-stack

--- pulumi/scripts/remove-stack.js
+++ pulumi/scripts/remove-stack.js
@@ -5,8 +5,11 @@ const { LocalWorkspace } = require("@pulumi/pulumi/automation");
 
 async function run() {
   const stackArgs = {
-    stackName: process.env.PR_STACK_NAME // the PR stackname
+    stackName: process.env.PR_STACK_NAME, // the PR stackname
+    workDir: path.join(__dirname, "../"), // the directory where our Pulumi.yaml exists
   };
+
+  const stack = await LocalWorkspace.createOrSelectStack(stackArgs);
 
   // remove the stack
   await LocalWorkspace.removeStack(stackArgs.stackName);
