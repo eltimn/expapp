6689be6ef1c6022f8988dff908b7d2e1c2501e33
Tim Nelson <tim@eltimn.com>
2022-06-20 15:09:29 -0500

    actual deploy

--- pulumi/index.js
+++ pulumi/index.js
@@ -13,23 +13,23 @@ const location = config.require("gcp_location");
 // Location to deploy Cloud Run services
 // const region = gcp.config.region || "us-central1";
 
-// const appService = new gcp.cloudrun.Service("expapp", {
-//   location,
-//   template: {
-//     spec: {
-//       containers: [
-//         { image: image_uri }
-//       ]
-//     }
-//   }
-// });
+const appService = new gcp.cloudrun.Service("expapp", {
+  location,
+  template: {
+    spec: {
+      containers: [
+        { image: image_uri }
+      ]
+    }
+  }
+});
 
-// const appIam = new gcp.cloudrun.IamMember("expapp-everyone", {
-//   service: appService.name,
-//   location,
-//   role: "roles/run.invoker",
-//   member: "allUsers"
-// });
+const appIam = new gcp.cloudrun.IamMember("expapp-everyone", {
+  service: appService.name,
+  location,
+  role: "roles/run.invoker",
+  member: "allUsers"
+});
 
 // Create a GCP resource (Storage Bucket)
 // const bucket = new gcp.storage.Bucket("my-bucket", {
@@ -37,8 +37,8 @@ const location = config.require("gcp_location");
 // });
 
 // Exports
-// exports.readme = appService.status.url;
-// exports.url = appService.status.url;
+exports.readme = appService.status.url;
+exports.url = appService.status.url;
 exports.image_uri = image_uri;
 exports.location = location;
 // exports.region = region;
